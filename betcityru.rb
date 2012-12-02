cf236780a841b663b95246bfcac518a04a6487c3
araslanov <araslanov_e_i@list.ru>
2012-12-03 03:18:17 +0600

    Edited of the debris

--- betcityru.rb
+++ betcityru.rb
@@ -14,15 +14,13 @@ FIELDS = %w{ TIME TEAM_1 HANDICAP_1 ODDS_1 TEAM_2 HANDICAP_2 ODDS_2 1 X 2 1X 12 
 
 a.get(LINK) do |page|
 
-  #puts FIELDS.index('TEAM_1')
-
   league_and_event = page.search('thead b').inner_text # имя лиги и события
 
   unless league_and_event.empty?
 
     league_and_event_array = league_and_event.split(SPLIT_FOR_LEAGUE_AND_EVENT) # разбиваем строку на массив
-    league = league_and_event_array.first # первый элемент массива - Имя лиги
-    event = league_and_event_array.drop(1).join(SPLIT_FOR_LEAGUE_AND_EVENT) # остальные элементы, кроме первого - Имя события
+    league = league_and_event_array.first # первый элемент массива - имя лиги
+    event = league_and_event_array.drop(1).join(SPLIT_FOR_LEAGUE_AND_EVENT) # остальные элементы, кроме первого - имя события
 
     puts "Имя лиги: #{league}"
     puts "Имя события: #{event}"
@@ -37,7 +35,7 @@ a.get(LINK) do |page|
       puts "Дата мероприятия: #{date_event.strftime("%d %B %Y")}"
 
       column_names_element = date.next_element # tbody.chead - наименования столбцов
-      column_names = column_names_element.search("td")
+      column_names = column_names_element.search("td") # массив наименований
       # вывод названий необходимых столбцов
       # можно и по одному прописать - column_names[FIELDS.index('TIME')]
       FIELDS.each_with_index do |field, index|
